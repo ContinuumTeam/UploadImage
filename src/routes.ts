5b45502936a72de642784df72aada16b961aeb7b
MatheusF99 <mc79846@gmail.com>
2021-02-13 09:29:02 -0300

    upload image configurated

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express'
 import multer from 'multer'
+
 import multerConfig from './config/multer'
+import Post from './models/Post'
+
 
 const routes = Router()
 
 routes.post(
-  '/posts', multer(multerConfig).single('file'), (req, res)=>{
-    console.log(req.file);
-    
-    return res.json({message: "hello world"})
+  '/posts', multer(multerConfig).single('file'), async (req, res)=>{
+    // console.log(req.file);
+    const post = await Post.create({
+      name: req.file.originalname,
+      size: req.file.size,
+      key: req.file.filename,
+      url:''
+    })
+
+    return res.json(post)
 
   }
 )
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from 'express'
 import routes from './routes'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/upload', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 app.use(express.json())
-
 app.use(express.urlencoded({ extended: true }))
-
 app.use(morgan('dev'))
-
 app.use(routes)
 
 app.listen(3333)
